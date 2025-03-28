6c60aa4f948ca591afd0f64e74767d0de8fe5264
pritisha6 <69625044+pritisha6@users.noreply.github.com>
2025-03-28 10:53:49 -0700

    Create groupbykey.py

---
+++ dataflow-python/groupbykey.py
@@ -0,0 +1,94 @@
+# Groupbykey - dataflow -python
+
+# step1: git clone https://github.com/GoogleCloudPlatform/training-data-analyst
+
+# step2: cd ~/training-data-analyst/courses/data_analysis/lab2/python
+
+# nano is_popular.py
+
+# Can you answer these questions about the file is_popular.py?
+
+# What custom arguments are defined?
+# What is the default output prefix?
+# How is the variable output_prefix in main() set?
+# How are the pipeline arguments such as --runner set?
+# What are the key steps in the pipeline?
+# Which of these steps happen in parallel?
+# Which of these steps are aggregations?
+
+# step3: How to execute the code?
+# python3 ./is_popular.py
+
+# #!/usr/bin/env python3
+
+# """
+# Copyright Google Inc. 2016
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# """
+
+import apache_beam as beam
+import argparse
+
+def startsWith(line, term):
+   if line.startswith(term):
+      yield line
+
+def splitPackageName(packageName):
+   """e.g. given com.example.appname.library.widgetname
+           returns com
+                   com.example
+                   com.example.appname
+      etc.
+   """
+   result = []
+   end = packageName.find('.')
+   while end > 0:
+      result.append(packageName[0:end])
+      end = packageName.find('.', end+1)
+   result.append(packageName)
+   return result
+   
+def getPackages(line, keyword):
+   start = line.find(keyword) + len(keyword)
+   end = line.find(';', start)
+   if start < end:
+      packageName = line[start:end].strip()
+      return splitPackageName(packageName)
+   return []
+
+def packageUse(line, keyword):
+   packages = getPackages(line, keyword)
+   for p in packages:
+      yield (p, 1)
+
+if __name__ == '__main__':
+   parser = argparse.ArgumentParser(description='Find the most used Java packages')
+   parser.add_argument('--output_prefix', default='/tmp/output', help='Output prefix')
+   parser.add_argument('--input', default='../javahelp/src/main/java/com/google/cloud/training/dataanalyst/javah>
+
+   options, pipeline_args = parser.parse_known_args()
+   p = beam.Pipeline(argv=pipeline_args)
+
+   input = '{0}*.java'.format(options.input)
+   output_prefix = options.output_prefix
+   keyword = 'import'
+
+   # find most used packages
+   (p
+      | 'GetJava' >> beam.io.ReadFromText(input)
+      | 'GetImports' >> beam.FlatMap(lambda line: startsWith(line, keyword))
+      | 'PackageUse' >> beam.FlatMap(lambda line: packageUse(line, keyword))
+      | 'TotalUse' >> beam.CombinePerKey(sum)
+      | 'Top_5' >> beam.transforms.combiners.Top.Of(5, key=lambda kv: kv[1])
+      | 'write' >> beam.io.WriteToText(output_prefix)
+   )
+   
+   p.run().wait_until_finish()
